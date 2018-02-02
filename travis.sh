cb0cd906da7dcfa91b63704ad9fb19227cb6eeff
Хорев Алексей Андреевич <khorevaa@art-vizing.ru>
2018-02-02 11:00:36 +0300

    тестирование через докер

---
+++ travis.sh
@@ -0,0 +1,14 @@
+
+
+
+wget -O os.deb http://oscript.io/downloads/1_0_19/onescript-engine_1.0.19_all.deb
+sudo dpkg -i *.deb; sudo apt install -f
+
+opm install 1testrunner; 
+opm install 1bdd; 
+
+opm install; 
+
+opm test; 
+
+
